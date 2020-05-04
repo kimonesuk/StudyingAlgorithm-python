fba4053ce4f671db21c00518c0656f9dcb09b013
OneSuk Kim <48847617+kimonesuk@users.noreply.github.com>
2020-05-04 22:28:01 +0900

    Create 7_A+B_7.py

---
+++ basic100/Step3_For/7_A+B_7.py
@@ -0,0 +1,11 @@
+t = int(input())
+a = []
+b = []
+for i in range(1,t+1):
+    alpha,beta = map(int,input().split())
+    a.append(alpha)
+    b.append(beta)
+for i in range(0, t):
+    num = i+1
+    sum = a[i]+b[i]
+    print(f'Case #{num}: {sum}')
