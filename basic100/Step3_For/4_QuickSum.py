8a833454accf0239c70a956d12f5c8d1867a132c
OneSuk Kim <48847617+kimonesuk@users.noreply.github.com>
2020-05-04 23:08:33 +0900

    Update 4_QuickSum.py

--- basic100/Step3_For/4_QuickSum.py
+++ basic100/Step3_For/4_QuickSum.py
@@ -1,1 +1,10 @@
-
+import sys
+t = int(input())
+a = []
+b = []
+for i in range(1,t+1):
+    alpha,beta = map(int,sys.stdin.readline().split())
+    a.append(alpha)
+    b.append(beta)
+for i in range(0, t):
+    print(a[i]+b[i])
